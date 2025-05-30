52015e86c80af95344cfcf68ad0151363543c461
Andy Waite <13400+andyw8@users.noreply.github.com>
2025-05-30 18:19:16 -0400

    Update default model since previous not longer on OpenRouter

--- lib/raix.rb
+++ lib/raix.rb
@@ -27,7 +27,7 @@ module Raix
     attr_accessor :openai_client
 
     DEFAULT_MAX_TOKENS = 1000
-    DEFAULT_MODEL = "meta-llama/llama-3-8b-instruct:free"
+    DEFAULT_MODEL = "meta-llama/llama-3.3-8b-instruct:free"
     DEFAULT_TEMPERATURE = 0.0
 
     # Initializes a new instance of the Configuration class with default values.
